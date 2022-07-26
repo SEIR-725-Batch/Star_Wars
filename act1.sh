58d714d515b1fe167c4a015c1de415ba8d9a0f6e
Brian Krabec <briankrabec@gmail.com>
2022-07-26 13:14:42 -0500

    Updates in act1 and act2

--- act1.sh
+++ act1.sh
@@ -1,7 +1,7 @@
 # 1. In this directory, create a new directory called star_wars. Example answer: mkdir star_wars
 mkdir star_wars
 # 2. In the star_wars folder, create two new directories: empire and rebellion (This can be done in two commands, but how would you do it in one?)
-
+I created two directories
 # 3. Inside the empire directory, create a file called darth_vader.txt 
 
 # 4. Use the force (or your echo) to add the text "...heavy breathing..." to the darth_vader.txt file (Don’t remember how to do this? Use the ‘other force’, known as Google!)
