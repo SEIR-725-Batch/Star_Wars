6590eff603844f84f62d595100a1c763286ab0de
“vvela2022” <victoriavela01@gmail.com>
2022-07-26 06:52:29 -0500

    add corrections to act 2

--- act2.sh
+++ act2.sh
@@ -11,3 +11,4 @@ touch han_solo.txt chewbacca.txt
 # 6. Move luke_skywalker, obi_wan, and princess_leia into the millenium_falcon, respectively.
  mv luke_skywalker.txt  obi_wan.txt  princess_leia.txt millenium_falcon
 # 7. Move the millenium_falcon into the death_star.
+mv rebellion/millenium_falcon empire/death_star
